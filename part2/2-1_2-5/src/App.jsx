e1cd28b495b00b5b36064ac2741f908c7d1de027
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-06-03 22:45:36 +0200

    2.1 (Step 6)

---
+++ part2/2-1_2-5/src/App.jsx
@@ -0,0 +1,58 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header text={course.name} />
+      <Content content={course} />
+    </>
+  );
+};
+
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Content = ({ content }) => {
+  return (
+    <>
+      {content.parts.map((part) => (
+        <Part key={part.id} content={part} />
+      ))}
+    </>
+  );
+};
+
+const Part = ({ content }) => {
+  return (
+    <p>
+      {content.name} {content.exercises}
+    </p>
+  );
+};

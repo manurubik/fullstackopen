00ee23ea2a0ea2c5e383af3adb3d4ceb8cc4428b
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-06-04 15:29:23 +0200

    2.6 (Step 1)

---
+++ part2/2-6_2-10/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInput = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName(""); // Clear the input field after adding
+  };
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;

4fbffb782062bc2fd13574b03cf985f271a301ac
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-06-11 02:22:24 +0200

    2.19 (Step 2)

--- part2/2-18_2-20/src/components/Countries.jsx
+++ part2/2-18_2-20/src/components/Countries.jsx
@@ -1,37 +1,55 @@
+import { useState, useEffect } from "react";
+
 const Countries = ({ countries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    setSelectedCountry(null);
+  }, [countries]);
+
+  const countryDetails = (country) => (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        width="200"
+      />
+    </div>
+  );
+
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
-  }
-
-  if (countries.length === 1) {
-    const country = countries[0];
+  } else if (countries.length === 1) {
+    return countryDetails(countries[0]);
+  } else if (selectedCountry) {
+    return countryDetails(selectedCountry);
+  } else {
     return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <p>capital {country.capital}</p>
-        <p>area {country.area}</p>
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(country.languages).map((language, index) => (
-            <li key={index}>{language}</li>
-          ))}
-        </ul>
-        <img
-          src={country.flags.png}
-          alt={`Flag of ${country.name.common}`}
-          width="200"
-        />
-      </div>
+      <ul>
+        {countries.map((country, index) => (
+          <li key={index}>
+            {country.name.common}{" "}
+            <button
+              onClick={() => {
+                setSelectedCountry(country);
+              }}
+            >
+              show
+            </button>
+          </li>
+        ))}
+      </ul>
     );
   }
-
-  return (
-    <ul>
-      {countries.map((country, index) => (
-        <li key={index}>{country.name.common}</li>
-      ))}
-    </ul>
-  );
 };
 
 export default Countries;

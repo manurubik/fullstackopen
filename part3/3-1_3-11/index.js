860e8836340d62131aed849128ee366c887c43a3
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-06-19 00:36:06 +0200

    3.4 (Step 4)

--- part3/3-1_3-11/index.js
+++ part3/3-1_3-11/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
-const persons = [
+let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -23,6 +23,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
